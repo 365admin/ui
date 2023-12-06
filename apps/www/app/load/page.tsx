c32c6517dea238782866b5f90396cfd80b3f815e
Niels Gregers Johansen <niels@jumpto365.com>
2023-12-06 13:27:48 +0100

    fix: link

--- apps/www/app/load/page.tsx
+++ apps/www/app/load/page.tsx
@@ -5,7 +5,7 @@ export default function RootPage() {
 
   const data = cookies().has("user") ? JSON.parse(cookies().get("user")?.value as string) : {}
   if (data.country && data.unit) {
-    redirect("https://christianiabpos.sharepoint.com/sites/nexiintra-home?country=" + data.country + "&unit=" + data.unit)
+    redirect("https://365admin.sharepoint.com/sites/365admin?country=" + data.country + "&unit=" + data.unit)
   } else[
     redirect("/")
   ]
